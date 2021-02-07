6b54567b092ba8412539f9ef2d40cedef208efad
ARJUNR950 <68820290+ARJUNR950@users.noreply.github.com>
2021-02-07 13:45:09 +0530

    Add files via upload

--- mp1.py
+++ mp1.py
@@ -2,7 +2,7 @@ import numpy as np       #for mathematical calculations
 import matplotlib.pyplot as plt #for plotting
 import pandas as pd      #for data frame creation
               
-df = pd.read_csv("/home/user/Desktop/project/output data/mp14.txt",sep="\t")
+df = pd.read_csv("/home/user/Desktop/project/output data/mp15.txt",sep="\t")
 X=np.array(df)         #convert df into array
 Xn=X[:,4]               #array of x component
 Yn=X[:,6]                #array of z component
@@ -28,7 +28,8 @@ n=len(Xn)
 X1 = []
 Y1 = []
 Z1  = []
-
+Xf=[]
+Yf=[]
 
 
 
@@ -40,11 +41,12 @@ t_y=(Yn[1]-Yn[0])/(Zn[1]-Zn[0])
 X_z=Xn[0]
 Y_z=Yn[0]
 Z_z =Zn[0]
+
 X1.append(X_z)
 Y1.append(Y_z)
 Z1.append(Z_z)
-
-
+Xf.append(X_z)
+Yf.append(Y_z)
 
 
 q_p=10**-4
@@ -61,7 +63,7 @@ By=0.0
 
 Bz = (0.0)
 
-
+K1=0.5
 #furthur points are find using prediction formulae
 #loop run from x[1] to x[n-1] i.e upto the last number in the array
 
@@ -94,20 +96,50 @@ for i in range(n-1): #loop for 150 ironplate+air gap combo (ignoring the first h
      h = (K*(q_p)*np.sqrt((1+(t_x*t_x)+(t_y*t_y))))
 
      X_z = X_z + (t_x*dz) + ( h * ( (t_x*t_y*S_x)-((1+(t_x*t_x))*S_y))) + ((h*h)*(((t_x*S_xx)*((3*(t_y*t_y))+1))-((t_y*S_xy)*((3*(t_x*t_x))+1))-((t_y*S_yx)*((3*(t_x*t_x))+1))+((t_x*S_yy)*((3*(t_x*t_x))+3))))
+     
+     
      Y_z = Y_z + (t_y*dz) + ( h * (((1+(t_y*t_y))*S_x)-(t_x*t_y*S_y) )) + ((h*h)*(((t_y*S_xx)*((3*(t_y*t_y))+3))-((t_x*S_xy)*((3*(t_y*t_y))+1))-((t_x*S_yx)*((3*(t_y*t_y))+1))+((t_y*S_yy)*((3*(t_x*t_x))+1))))
+     
      t_x   = t_x +             ( h * ( (t_x*t_y*R_x)-((1+(t_x*t_x))*R_y))) + ((h*h)*(((t_x*R_xx)*((3*(t_y*t_y))+1))-((t_y*R_xy)*((3*(t_x*t_x))+1))-((t_y*R_yx)*((3*(t_x*t_x))+1))+((t_x*R_yy)*((3*(t_x*t_x))+3))))
      t_y   = t_y +             ( h * (((1+(t_y*t_y))*R_x)-(t_x*t_y*R_y) )) + ((h*h)*(((t_y*R_xx)*((3*(t_y*t_y))+3))-((t_x*R_xy)*((3*(t_y*t_y))+1))-((t_x*R_yx)*((3*(t_y*t_y))+1))+((t_y*R_yy)*((3*(t_x*t_x))+1))))  
+    
 
 
      
 #adding elements to X1 and Y1
-     X1 . append  (X_z)
-     Y1 . append  (Y_z)   
-     Z1 . append  (Z_z)
+    
+     X_2=(1-K1)*X_z+K1*Xn[i]
+     Y_2= (1-K1)*Y_z+K1*Yn[i]
+     
+
+    Yf.append(Y_2)
+    Xf.append(X_2)
+
+     
+    X1 . append  (X_z)
+    Y1 . append  (Y_z)   
+    Z1 . append  (Z_z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 plt.grid(b=None, which='major', axis='both')
 
 #scatter plot
-xval=plt.plot(Y1,Z1,label='posx prediction (mm)')
+xval=plt.plot(Yf,Z1,label='posx prediction (mm)')
 #labelling axis
 plt.xlabel('pos(mm)')
 plt.ylabel('posz(mm)')
@@ -115,14 +147,14 @@ plt.ylabel('posz(mm)')
 yval=plt.plot(Yn,Zn,label='posy prediction (mm)')
 
 #giving title to the graph
-plt.title(' plot obtained from points using prediction formula (14Gev)')
+plt.title(' plot obtained from points using prediction formula (15Gev)')
 
 
 #naming the data sets,getting label on plot
 plt.legend(["Y predict vs Z", "Y vs Z"])
 plt.show()
 
-xval=plt.plot(X1,Z1,label='posx prediction (mm)')
+xval=plt.plot(Xf,Z1,label='posx prediction (mm)')
 #labelling axis
 plt.xlabel('pos(mm)')
 plt.ylabel('posz(mm)')
@@ -130,17 +162,14 @@ plt.ylabel('posz(mm)')
 yval=plt.plot(Xn,Zn,label='posy prediction (mm)')
 
 #giving title to the graph
-plt.title(' plot obtained from points using prediction formula (14Gev)')
+plt.title(' plot obtained from points using prediction formula (15Gev)')
 
 
 #naming the data sets,getting label on plot
 plt.legend(["X predict vs Z", "X vs Z"])
+
+
 plt.show()
-
-
-
-
-
 
 
 
